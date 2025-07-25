3212faae5bf979ea7d0c2e033f24264dfdbac8d9
Omar  Karake <omar.karake@amalitech.com>
2025-07-25 09:57:56 +0200

    failing tests using fail()

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,8 @@
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
     pending(); // for telling jasmine that we are not yet ready for testing this specification.
+  });
+  it('should subtract two numbers', () => {
+    fail(); // we mark it as fail, if we want to write test case that will fail
   })
 })
